9f3a093ee87460ed85914cc424b590bbba131dac
ignaciosoraka <ignaciosoraka@gmail.com>
2024-10-09 14:32:46 -0300

    arreglos

--- Assets/java/nav.js
+++ Assets/java/nav.js
@@ -3,12 +3,12 @@ const toggleMenu = () => {
     const burgerMenu = document.querySelector(".menu-icon");
     const src = burgerMenu.getAttribute("src");
   
-    // Verifica si contiene "burger-menu.svg" en la ruta, sin importar si es "Assets" o "../Assets"
+    // Cambia el ícono del menú hamburguesa
     const isBurger = src.includes("burger-menu.svg");
     const iconName = isBurger ? src.replace("burger-menu.svg", "close.svg") : src.replace("close.svg", "burger-menu.svg");
-
     burgerMenu.setAttribute("src", iconName);
 
+    // Mostrar o esconder el menú principal
     if (!isBurger) {
         navigation.classList.add("navigation--mobile--fadeout");
         setTimeout(() => {
@@ -20,13 +20,36 @@ const toggleMenu = () => {
     }
 };
 
-// Añadir evento para cerrar el menú al hacer clic en un enlace de navegación
-document.querySelectorAll('.navigation a').forEach(link => {
-    link.addEventListener('click', () => {
-        const navigation = document.querySelector(".navigation");
+// Evento para abrir/cerrar submenús al hacer clic en elementos con submenú
+document.querySelectorAll('.navigation > li:has(ul) > a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        // Evitar redirección
+        event.preventDefault();
+        
+        // Evitar cerrar el menú si el enlace tiene un submenú
+        event.stopPropagation();
+        
+        const parentLi = event.target.parentElement;
 
-        // Solo cerrar el menú si está abierto
-        if (navigation.classList.contains("navigation--mobile")) {
+        // Alternar el submenú
+        parentLi.classList.toggle('open-submenu');
+
+        // Alternar flecha de submenú
+        const arrow = parentLi.querySelector('.dropdown-arrow');
+        if (arrow) {
+            arrow.classList.toggle('rotate-arrow');
+        }
+    });
+});
+
+// Evento para cerrar el menú al hacer clic en un enlace sin submenú
+document.querySelectorAll('.navigation a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        const parentLi = event.target.closest('li');
+        const hasSubmenu = parentLi && parentLi.querySelector('.submenu');
+        
+        // Si el enlace no tiene submenú, cierra el menú móvil
+        if (!hasSubmenu) {
             toggleMenu();
         }
     });
