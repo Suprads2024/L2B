7473480b4344ebba833cc4187bb7feb5cf97b03a
ignaciosoraka <ignaciosoraka@gmail.com>
2024-11-20 18:00:04 -0300

    arquitecto

--- Assets/java/scrolnav.js
+++ Assets/java/scrolnav.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function() {
-  const menuBar = document.querySelector('.menu__bar');
+  const menuBar = document.querySelector('.menu__bar, .menu__bar2');
   if (window.scrollY > 50) {  // Cambia '50' por la cantidad de scroll en píxeles donde quieres que se active
       menuBar.classList.add('scrolled');
   } else {
